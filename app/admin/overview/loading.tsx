50e9d5e2f995c0431479b8f431d7a5ba06bd36cb
Zuhaib Hanfi <hanfizuhaib@gmail.com>
2025-09-14 02:49:50 +0530

    final tweaks

--- app/admin/overview/loading.tsx
+++ app/admin/overview/loading.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const LoadingPage = () => {
   return (
     <div className='flex items-center justify-center min-h-screen'>
-      <Loader2 className='h-12 w-12 animate-spin text-emerald-500' />
+      <Loader2 className='h-12 w-12 animate-spin text-amber-500' />
     </div>
   )
 }
