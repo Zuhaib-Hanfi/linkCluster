92928e6a8f6ac78f45ab2c741758018beea10c23
Zuhaib Hanfi <hanfizuhaib@gmail.com>
2025-09-16 12:05:16 +0530

    added analyticsc

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
